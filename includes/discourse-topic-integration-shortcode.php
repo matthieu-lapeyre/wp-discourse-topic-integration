ccab06d26f84fef7141cfb470873fda8e36e9de3
Matthieu Lapeyre <matthieulapeyre@gmail.com>
2015-06-19 13:02:24 +0200

    finally remove angularjs and go for full-php

--- includes/discourse-topic-integration-shortcode.php
+++ includes/discourse-topic-integration-shortcode.php
@@ -5,32 +5,21 @@ add_shortcode( 'discourse', 'integrate_discourse_topic' );
 
 function integrate_discourse_topic( $atts, $content = null ) {
     $a = shortcode_atts( array(
-        'attr_1' => 'attribute 1 default',
+        'topic' => 'attribute 1 default',
         'attr_2' => 'attribute 2 default',
         // ...etc
     ), $atts );
 
-    // Trick from https://themeavenue.net/using-html-shortcode-function/
-    /* Turn on buffering */
-    	ob_start(); ?>
+    $json_string = file_get_contents($atts['topic']);
+    $parsed_json = json_decode($json_string, true);
 
-      <div ng-app="DiscourseApp">
-        <div class="main" ng-controller="MainController">
-            <h1>{{ project_name }}</h1>
-            <h2> {{author}}</h2>
-            <div ng-bind-html="topic_content | sanitize"></div>
-        </div>
-      </div>
+    $title = $parsed_json['title'];
+    $author = $parsed_json['details']['created_by']['username'];
+    $content = $parsed_json['post_stream']['posts'][0]['cooked'];
 
-    	<?php
-    	/* Get the buffered content into a var */
-    	$sc = ob_get_contents();
-
-    	/* Clean buffer */
-    	ob_end_clean();
-
-    	/* Return the content as usual */
-    	return $sc;
+    echo '<h1>'.$title.'</h1>';
+    echo '<h2>'.$author.'</h2>';
+    echo $content;
 
 }
 
