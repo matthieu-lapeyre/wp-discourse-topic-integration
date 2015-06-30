f4b0684337017b879b2b4c2fc2558cba34a1e328
Matthieu Lapeyre <matthieulapeyre@gmail.com>
2015-06-30 23:00:47 +0200

    remove author info on the subject template

--- templates/topic_subject.php
+++ templates/topic_subject.php
@@ -1,7 +1,5 @@
 <div class="discourse-topic-integration">
   <h1><?php echo $title; ?></h1>
-  <h2><?php echo $author; ?></h2>
-  <?php echo '<img src="'.$author_avatar.'" >'; ?>
   <?php echo $content; ?>
   <hr/>
   <?php echo '<a href="'.$discourse_base_url . '/t/'. $atts['topic_id'] .'">'; ?> <button> Go to the discussion</button> </a>
